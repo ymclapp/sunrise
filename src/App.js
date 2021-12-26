7b7e5deebd157ef41d5d8a86dc76e0d9bfbc06e1
ymcla <ymclapp@yahoo.com>
2021-12-26 16:57:17 -0600

    added okta and am working through the api.  Stuck on getting clientid from okta

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Search } from './components/Search';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
+import { Home } from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        Books with Hooks
-      </header>
-      <Search />
+      <Router>
+        <header>
+          <div>Books with Hooks</div>
+          <ul className="menu"><li><Link to="/">Home</Link></li><li><Link to="/search">Search</Link></li></ul>
+        </header>
+        <Security issuer='https://dev-69718841.okta.com/'
+        <Search />
+      </Router>
     </div>
   );
 }
--- src/ProjectREADME.md
+++ src/ProjectREADME.md
@@ -1,7 +1,13 @@
 
+###  Referenced
+-  https://developer.okta.com/blog/2020/08/26/react-hooks
+
 ### Dependencies
 1.  React
 2.  React Bootstrap
+3.  Okta
+4.  okta-react@3.0.4 
+5.  react-router-dom@5.2.0
 
 ###  File Structure
 react-sunrise-app
